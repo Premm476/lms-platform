88b61820967f75f64b00687027907b7bf610be29
Premm476 <premm@techspira.co.in>
2025-03-14 14:59:08 +0530

    Updated LMS platform with latest changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,13 @@
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./utils/**/*.{js,ts,jsx,tsx}",
+  ],
+  safelist: [
+    'bg-blue-500',
+    'text-red-500',
+    // Add other dynamic classes here
   ],
   theme: {
     extend: {},
